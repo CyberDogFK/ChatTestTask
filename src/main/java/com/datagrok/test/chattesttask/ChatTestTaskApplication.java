25d9ccf1e1f0b74900df49e668766c77e47e462d
AntonPav <streetreadger@gmail.com>
2023-01-16 14:57:08 +0200

    project Init

---
+++ src/main/java/com/datagrok/test/chattesttask/ChatTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.datagrok.test.chattesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datagrok/test/chattesttask/ChatTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datagrok.test.chattesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
